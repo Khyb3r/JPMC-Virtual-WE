6e507c4ca3f9e05e7887e98e8bd37c1bf1a90f6c
Khyb3r <157541714+Khyb3r@users.noreply.github.com>
2025-09-14 21:11:46 +0100

    Added external incentive API handling and fixed atomicity error in database operations in 'savetransactions' method

---
+++ src/main/java/com/jpmc/midascore/transactionincentive/Incentive.java
@@ -0,0 +1,8 @@
+package com.jpmc.midascore.transactionincentive;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Transaction {
+    
+}
